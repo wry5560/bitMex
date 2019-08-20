038feab4385b1c067095c920ecdac427183bd810
wry5560 <233089043@qq.com>
2019-08-20 23:56:15 +0800

    socket

--- routes/logApi.js
+++ routes/logApi.js
@@ -1,4 +1,5 @@
 var logger = require('../lib/log4js').celveLogger;
+import websocketlogControl from '../moudles/logs/websocketlogs.server.controllar'
 
 var express = require('express');
 var router = express.Router();
@@ -7,4 +8,17 @@ router.post('/',async function(req, res, next) {
     console.log(req.body)
     logger.info(req.body)
 });
+
+router.post('/websocket',async function(req, res, next) {
+    // console.log(req.body)
+    const {message}=req.body
+    try{
+        const data = await websocketlogControl.createWebsocketlog(message)
+        res.send({data,success:true});
+    }catch(e) {
+        res.send({message:e,success:false});
+    }
+    logger.info(req.body)
+});
+
 module.exports = router;
