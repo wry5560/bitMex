d708fadb383977e4e49197971728619d403a112f
wry5560 <233089043@qq.com>
2019-08-27 17:04:30 +0800

    bitmex

--- app.js
+++ app.js
@@ -47,8 +47,8 @@ app.use(session({
   },
 }));
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({ extended: false,limit: '50mb' }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
